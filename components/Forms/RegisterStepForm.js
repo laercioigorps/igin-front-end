d74e358677faeefeb6b6dbba1260a74c07cae9c2
laercio <laercioigorps@gmail.com>
2021-09-11 06:07:44 -0300

    formatedg/goals/steps/gddgdg

--- components/Forms/RegisterStepForm.js
+++ components/Forms/RegisterStepForm.js
@@ -1,41 +1,61 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 //import useGoals from "../../data/use-goals";
 
-export default function RegisterStepForm(props){
+export default function RegisterStepForm(props) {
+  // const { data, loading, loggedOut, error} = useGoals();
+  //   console.log(data)
+  //   console.log("locading: "+ loading)
+  //   console.log("loggedOut:" +loggedOut)
 
-	// const { data, loading, loggedOut, error} = useGoals();
-	//   console.log(data)
-	//   console.log("locading: "+ loading)
-	//   console.log("loggedOut:" +loggedOut)
-	
-
-	return(
-		<div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
-		<form onSubmit={props.onClick}>
-		    <div className="mb-4">
-		      <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="name">
-		        Step
-		      </label>
-		      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="name" type="text" placeholder="your step here"/>
-		    </div>
-		    <div className="mb-6">
-		      <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="description">
-		        Description
-		      </label>
-		      <textarea className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="description" type="password" placeholder=""> </textarea>
-		      <p className="text-red text-xs italic">{props.error}</p>
-		    </div>
-		    <div className="flex items-center justify-between">
-		      <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" type="submit">
-		        Sign In
-		      </button>
-		      <a className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="#">
-		        Forgot Password?
-		      </a>
-		    </div>
-		</form>
-		</div>
-
-	)
-
+  return (
+    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+      <form onSubmit={props.onClick}>
+        <div className="mb-4">
+          <label
+            className="block text-grey-darker text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            Step
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+            id="name"
+            type="text"
+            placeholder="your step here"
+            defaultValue={props.step ? props.step.name : null}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-grey-darker text-sm font-bold mb-2"
+            htmlFor="description"
+          >
+            Description
+          </label>
+          <textarea
+            className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+            id="description"
+            type="password"
+            placeholder=""
+            defaultValue={props.step ? props.step.description : null}
+          ></textarea>
+          <p className="text-red text-xs italic">{props.error}</p>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            Sign In
+          </button>
+          <a
+            className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker"
+            href="#"
+          >
+            Forgot Password?
+          </a>
+        </div>
+      </form>
+    </div>
+  );
 }
