8fd41526677265fa49aea1229b6fb3c19b039928
laercio <laercioigorps@gmail.com>
2021-09-14 05:19:51 -0300

    changed component CardTaskList to CardTasksByIterationList

--- pages/iterations.js
+++ pages/iterations.js
@@ -1,40 +1,26 @@
 import React from "react";
 
 // components
-import Layout from '../components/layout.js'
+import Layout from "../components/layout.js";
 // import CardLineChart from "../components/Cards/CardLineChart.js";
 // import CardBarChart from "../components/Cards/CardBarChart.js";
 import CardIterationList from "../components/Cards/CardIterationList.js";
 import CardTasksList from "../components/Cards/CardTasksList.js";
+import CardTasksByIterationList from "../components/Cards/CardTasksByIterationList.js";
 
 export default function Goals() {
-
-  const tasks = [
-  {"id":1,"name":"wake up", "description": "my Hy", "completed": "2021-08-02", "goal":1},
-  {"id":2,"name":"take a shower", "description": "my description", "completed": "2021-08-02", "goal":1},
-  {"id":3,"name":"sleep", "description": "my description", "completed": "2021-08-02", "goal":2},
-  {"id":4,"name":"eat",  "description": "my description", "completed": "2021-08-02", "goal":"become a software developer"},
-  ]
-
-  const iterations = [
-    {"id":1,"number":"1", "description": "my Hy", "date": "2021-08-02", "completed":'true'},
-    {"id":2,"number":"2", "description": "my description", "date": "2021-08-02", "completed":'true'},
-    {"id":3,"number":"3", "description": "my description", "date": "2021-08-02", "completed": "false"},
-    {"id":4,"number":"4",  "description": "my description", "date": "2021-08-02", "completed":"true"},
-  ]
   return (
     <>
-    <Layout card=''>
-      <div className="flex flex-wrap mt-4">
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardTasksList tasks={tasks}/>
+      <Layout card="">
+        <div className="flex flex-wrap mt-4">
+          <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+            <CardTasksByIterationList />
+          </div>
+          <div className="w-full xl:w-4/12 px-4">
+            <CardIterationList />
+          </div>
         </div>
-        <div className="w-full xl:w-4/12 px-4">
-        
-          <CardIterationList/>
-        </div>
-      </div>
-      {/*<div className="flex flex-wrap">
+        {/*<div className="flex flex-wrap">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
           <CardLineChart />
         </div>
@@ -42,7 +28,7 @@ export default function Goals() {
           <CardBarChart />
         </div>
       </div>*/}
-    </Layout>
+      </Layout>
     </>
   );
 }
