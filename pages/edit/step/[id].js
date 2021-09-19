daba7bcf6aaae3b9c407c655ce3d53853835cd05
laercio <laercioigorps@gmail.com>
2021-09-19 07:23:21 -0300

    implemented delete step button

--- pages/edit/step/[id].js
+++ pages/edit/step/[id].js
@@ -2,21 +2,23 @@ import RegisterStepForm from "../../../components/Forms/RegisterStepForm";
 import Layout from "../../../components/layout.js";
 import { useRouter } from "next/router";
 import React from "react";
-import {useNeeds, useStep} from "../../../data/use-data";
+import { useNeeds, useStep } from "../../../data/use-data";
+import DeleteButton from "../../../components/Button/DeleteButton";
+
 
 export default function EditStep() {
   const router = useRouter();
   const { id } = router.query;
   const { step, stepLoading, stepError } = useStep(id ? id : null);
-  console.log("aiaiai")
-  console.log(step)
+  console.log("aiaiai");
+  console.log(step);
   var error = "";
 
   const edit = async (event) => {
     event.preventDefault(); // don't redirect the page
     // where we'll add our form logic
 
-    const res = await fetch("http://127.0.0.1:8000/step/"+id+"/", {
+    const res = await fetch("http://127.0.0.1:8000/step/" + id + "/", {
       body: JSON.stringify({
         name: event.target.name.value,
         description: event.target.description.value,
@@ -35,14 +37,30 @@ export default function EditStep() {
     console.log(result);
     if (result.id) {
       router.push({
-        pathname: '/goals/steps/[id]',
+        pathname: "/goals/steps/[id]",
         query: { id: step.id },
-      })
-      
+      });
     } else {
       console.log(result.need);
       error = result.need;
     }
+  };
+
+  const deleteStep = async (event) => {
+    event.preventDefault(); // don't redirect the page
+    // where we'll add our form logic
+
+    const res = await fetch("http://127.0.0.1:8000/step/" + id + "/", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Token " + localStorage.token,
+      },
+      method: "DELETE",
+    });
+    router.push({
+      pathname: "/goals/[id]",
+      query: { id: step.goal },
+    });
   };
 
   return (
@@ -50,6 +68,9 @@ export default function EditStep() {
       <Layout cond="false" card="off">
         <div className="relative flex flex-col min-w-0 break-words bg-white mb-6 shadow-lg rounded lg:w-2/5 mx-auto">
           <RegisterStepForm onClick={edit} error={error} step={step} />
+        </div>
+        <div className="relative flex flex-col min-w-0 break-words bg-white mb-6 shadow-lg rounded lg:w-2/5 mx-auto">
+          <DeleteButton onClick={deleteStep} />
         </div>
       </Layout>
     </>
