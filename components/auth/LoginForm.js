e7e2cb4a5505aabf124dec0ff97c389ba0374acf
laercio <laercioigorps@gmail.com>
2021-10-08 06:04:49 -0300

    login form login name fixed and register link added

--- components/auth/LoginForm.js
+++ components/auth/LoginForm.js
@@ -1,39 +1,75 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function LoginForm(props) {
-	return (
-		<div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
-			<form onSubmit={props.onClick}>
-				<div className="mb-4">
-					<label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="username">
-						Username
-					</label>
-					<input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" placeholder="Username" />
-				</div>
-				<div className="mb-6">
-					<label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="password">
-						Password
-					</label>
-					<input className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="******************" />
-					{props.passwordError ? props.passwordError.map((erro, index) =>
-						<p key={index} className="text-red-500 text-xs italic">{erro}</p>
-					) : null}
-					{props.nonFieldError ? props.nonFieldError.map((erro, index) =>
-						<p key={index} className="text-red-500 text-xs italic">{erro}</p>
-					) : null}
-
-				</div>
-				<div className="flex items-center justify-between">
-					<button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" type="submit">
-						Sign In
-					</button>
-					<a className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="#">
-						Forgot Password?
-					</a>
-				</div>
-			</form>
-		</div>
-
-	)
-
+  return (
+    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+      <form onSubmit={props.onClick}>
+        <div className="mb-4">
+          <label
+            className="block text-grey-darker text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+            id="username"
+            type="text"
+            placeholder="Username"
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-grey-darker text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+            id="password"
+            type="password"
+            placeholder="******************"
+          />
+          {props.passwordError
+            ? props.passwordError.map((erro, index) => (
+                <p key={index} className="text-red-500 text-xs italic">
+                  {erro}
+                </p>
+              ))
+            : null}
+          {props.nonFieldError
+            ? props.nonFieldError.map((erro, index) => (
+                <p key={index} className="text-red-500 text-xs italic">
+                  {erro}
+                </p>
+              ))
+            : null}
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            Log In
+          </button>
+          <a
+            className="inline-block align-baseline font-bold text-sm text-blue-400 hover:text-blue-700"
+            href="#"
+          >
+            Forgot Password?
+          </a>
+        </div>
+        <Link href={props.registerPage ? props.registerPage : "#"}>
+          <a
+            className="text-right block align-baseline font-bold text-sm text-blue-400 hover:text-blue-700"
+            
+          >
+            or Doesn't have an account?
+          </a>
+        </Link>
+      </form>
+    </div>
+  );
 }
