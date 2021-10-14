88ce4330f0ff046a0fdd4c24de192fb3aea771b0
laercio <laercioigorps@gmail.com>
2021-10-14 06:50:36 -0300

    added fields to form that loads when task in usetask is defined: step, iteration, completed

--- components/Forms/RegisterTaskForm.js
+++ components/Forms/RegisterTaskForm.js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 //import useGoals from "../../data/use-goals";
 
-export default function RegisterStepForm(props) {
+export default function RegisterTaskForm(props) {
   // const { data, loading, loggedOut, error} = useGoals();
   //   console.log(data)
   //   console.log("locading: "+ loading)
@@ -22,6 +22,7 @@ export default function RegisterStepForm(props) {
             id="name"
             type="text"
             placeholder="your step here"
+            defaultValue={props.task ? props.task.name : ""}
           />
         </div>
         <div className="mb-6">
@@ -36,11 +37,61 @@ export default function RegisterStepForm(props) {
             id="description"
             type="password"
             placeholder=""
-          >
-            {" "}
-          </textarea>
+            defaultValue={props.task ? props.task.description : ""}
+          ></textarea>
           <p className="text-red text-xs italic">{props.error}</p>
         </div>
+
+        {props.task ? (
+          <>
+            <div className="mb-4">
+              <label
+                className="block text-grey-darker text-sm font-bold mb-2"
+                htmlFor="step"
+              >
+                Step
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                id="step"
+                type="text"
+                placeholder="your step here"
+                defaultValue={props.task ? props.task.step : ""}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-grey-darker text-sm font-bold mb-2"
+                htmlFor="name"
+              >
+                Iteration
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                id="name"
+                type="text"
+                placeholder="your step here"
+                defaultValue={props.task ? props.task.iteration : ""}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-grey-darker text-sm font-bold mb-2"
+                htmlFor="completed"
+              >
+                Completed
+              </label>
+              <input
+                className=""
+                id="completed"
+                type="checkbox"
+                defaultChecked={props.task ? props.task.iteration : ""}
+              />
+            </div>{" "}
+          </>
+        ) : (
+          ""
+        )}
         <div className="flex items-center justify-between">
           <button
             className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mx-auto"
