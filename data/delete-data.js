91fd0d4b3fdcfe0ee4de5939a6cc42d0206e3e2f
laercio <laercioigorps@gmail.com>
2021-11-14 16:03:01 -0300

    added function to delete step

--- data/delete-data.js
+++ data/delete-data.js
@@ -1,12 +1,23 @@
 export const deleteGoal = async (id) => {
+  const res = await fetch("http://127.0.0.1:8000/goal/" + id + "/", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Token " + localStorage.token,
+    },
+    method: "DELETE",
+  });
+  const result = await res.json();
+  return result;
+};
 
-    const res = await fetch("http://127.0.0.1:8000/goal/" + id + "/", {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Token " + localStorage.token,
-      },
-      method: "DELETE",
-    });
-    const result = await res.json();
-    return result
-  };
+export const deleteStep = async (id) => {
+  const res = await fetch("http://127.0.0.1:8000/step/" + id + "/", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Token " + localStorage.token,
+    },
+    method: "DELETE",
+  });
+  const result = await res.json();
+  return result;
+};
