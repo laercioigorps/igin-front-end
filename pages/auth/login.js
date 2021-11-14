fe7e113d2ecb2fae5e81194089376d9e2c079531
laercio <laercioigorps@gmail.com>
2021-11-14 11:45:29 -0300

    updated page to use user login function in data/user.js

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -2,6 +2,7 @@ import LoginForm from "../../components/auth/LoginForm.js";
 import Layout from '../../components/layout.js'
 import { useRouter } from 'next/router'
 import React, { useState } from 'react';
+import {login} from "../../data/user"
 
 
 export default function Login(){
@@ -14,25 +15,9 @@ export default function Login(){
 	const logUser = async event => {
 	    event.preventDefault() // don't redirect the page
 	    // where we'll add our form logic
-	  
 
-	    const res = await fetch(
-	      'http://127.0.0.1:8000/rest-auth/login/',
-	      {
-	        body: JSON.stringify({
-	          username: event.target.username.value,
-	          password: event.target.password.value,
-	        }),
-	        headers: {
-	          'Content-Type': 'application/json'
-	        },
-	        method: 'POST'
-	      }
-	    )
-
-	    const result = await res.json()
+	    const result = await login(event.target.username.value, event.target.password.value )
 	    
-	    console.log(result)
 	    if(result.key){
 		    console.log(result.key)
 		    localStorage.setItem('token', result.key)
@@ -53,21 +38,21 @@ export default function Login(){
 		}
 
 
-	}
+	} 
 	
 
-		return (
-			<>
-		    <Layout cond='false' card="off">
+	return (
+		<>
+		<Layout cond='false' card="off">
 
-		      <div className="relative flex flex-col min-w-0 break-words bg-white mb-6 shadow-lg rounded lg:w-2/5 mx-auto">
-		        <LoginForm onClick = {logUser} passwordError={passwordError} nonFieldError={nonFieldError} registerPage="/auth/register"/>
-		      </div>
-		      
-		    </Layout>
-		    </>
-		);
-	}
+			<div className="relative flex flex-col min-w-0 break-words bg-white mb-6 shadow-lg rounded lg:w-2/5 mx-auto">
+			<LoginForm onClick = {logUser} passwordError={passwordError} nonFieldError={nonFieldError} registerPage="/auth/register"/>
+			</div>
+			
+		</Layout>
+		</>
+	);
+}
 
 	
 	
