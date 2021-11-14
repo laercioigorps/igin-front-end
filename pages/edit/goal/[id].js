ca9ca49defee623cb8fd72d79ab88dac5757e077
laercio <laercioigorps@gmail.com>
2021-11-14 15:35:16 -0300

    update to use edit goal from edit-data.js

--- pages/edit/goal/[id].js
+++ pages/edit/goal/[id].js
@@ -4,40 +4,28 @@ import { useRouter } from "next/router";
 import React from "react";
 import { useNeeds, useGoal } from "../../../data/use-data";
 import DeleteButton from "../../../components/Button/DeleteButton";
+import {editGoal} from "../../../data/edit-data"
 
 export default function EditGoal() {
   const router = useRouter();
   const { id } = router.query;
   const { goal, goalLoading, goalError } = useGoal(id ? id : null);
-  console.log("aiaiai");
-  console.log(goal);
   var error = "";
 
   const edit = async (event) => {
     event.preventDefault(); // don't redirect the page
     // where we'll add our form logic
 
-    const res = await fetch("http://127.0.0.1:8000/goal/" + id + "/", {
-      body: JSON.stringify({
-        name: event.target.name.value,
-        description: event.target.description.value,
-        endDate: event.target.endDate.value,
-        need: event.target.need.value,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Token " + localStorage.token,
-      },
-      method: "PUT",
-    });
+    const name = event.target.name.value
+    const description = event.target.description.value
+    const endDate = event.target.endDate.value
+    const need = event.target.need.value
 
-    const result = await res.json();
+    const result = await editGoal(id, name, description, endDate, need)
 
-    console.log(result);
     if (result.id) {
       router.push("/goals/" + result.id);
     } else {
-      console.log(result.need);
       error = result.need;
     }
   };
