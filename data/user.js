85e9e3d227f665392c7a4ff8db68a24dae27c735
laercio <laercioigorps@gmail.com>
2021-11-15 10:14:34 -0300

    added registerUser function

--- data/user.js
+++ data/user.js
@@ -13,3 +13,21 @@ export const login = async (login, password) => {
   const result = await res.json();
   return result;
 };
+
+export const RegisterUser = async (username, email, password1, password2) => {
+  const res = await fetch("http://127.0.0.1:8000/rest-auth/registration/", {
+    body: JSON.stringify({
+      username: username,
+      email: email,
+      password1: password1,
+      password2: password2,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  });
+
+  const result = await res.json();
+  return result;
+};
