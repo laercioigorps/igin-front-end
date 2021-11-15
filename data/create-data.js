e10a8d7a576c3755b9b448c15555a08529cbf9e5
laercio <laercioigorps@gmail.com>
2021-11-15 08:47:34 -0300

    added create step and create goal

--- data/create-data.js
+++ data/create-data.js
@@ -16,3 +16,23 @@ export const registerStep = async (name, description, completed, goal) => {
   const resp = await res.json();
   return resp;
 };
+
+export const registerGoal = async (name, description, endDate, need) => {
+  const res = await fetch("http://127.0.0.1:8000/goal/", {
+    body: JSON.stringify({
+      name: name,
+      description: description,
+      endDate: endDate,
+      need: need,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Token " + localStorage.token,
+    },
+    method: "POST",
+  });
+
+  const result = await res.json();
+
+  return result;
+};
