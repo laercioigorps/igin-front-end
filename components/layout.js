479d8cbe1c141ffbe1a0a8f0e2fefcdf671b6494
laercio <laercioigorps@gmail.com>
2021-11-17 17:36:31 -0300

    update layout component to use a prop called 'loginRequired', then will check if true will redirect to login page if the user its not loged

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 
 // components
@@ -7,10 +7,15 @@ import AdminNavbar from "../components/Navbars/AdminNavbar.js";
 import Sidebar from "../components/Sidebar/Sidebar.js";
 import HeaderStats from "../components/Headers/HeaderStats.js";
 import FooterAdmin from "../components/Footers/FooterAdmin.js";
+import { useUser } from "../data/use-data.js";
+import { useRouter } from "next/router";
 
 
 
-export default function Layout({ children, card, statsList, wizard}) {
+export default function Layout({ children, card, statsList, wizard, loginRequired}) {
+  const { user, loading, loggedOut } = useUser();
+  const router = useRouter();
+  useEffect(() => (loggedOut && loginRequired ? router.push("/auth/login") : null));
   return (
     <>
       <Sidebar />
